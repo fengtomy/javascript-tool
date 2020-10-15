4b15250f8a91aa5ef84134cdd7077559fc16f50c
fengxh <shangfxh@gmail.com>
2020-10-15 18:20:00 +0800

    add: getArrByKey

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-function returnValueOfSpecificProp(prop) {
+function returnValueOfSpecificProp(prop, callback) {
   return function returnValue(single) {
+    if (typeof callback === "function") return callback(single[prop], single, prop);
     return single[prop];
   }
 }
 
+function getArrByKey(key, callback) {
+  const returnValue = returnValueOfSpecificProp(key, callback);
+  return function returnArr(source) {
+    return source.map(returnValue);
+  }
+}
+
 module.exports = {
-  returnValueOfSpecificProp
+  returnValueOfSpecificProp,
+  getArrByKey
 };
--- use-cases.js
+++ use-cases.js
@@ -1,4 +1,7 @@
-const { returnValueOfSpecificProp } = require("./index");
+const {
+  returnValueOfSpecificProp,
+  getArrByKey
+} = require("./index");
 
 // return age of a cat
 const returnAge = returnValueOfSpecificProp("age");
@@ -7,5 +10,10 @@ console.log(age); // 10
 
 // return ageList of a cat array
 const catList = [{ name: "catA", age: 10 }, { name: "catB", age: 20 }];
-const ageList = catList.map(returnAge);
-console.log(ageList); // [10, 20];
+const returnStatement = returnValueOfSpecificProp("age", (age, single, prop) => `${single.name}'s ${prop} is ${age}`);
+const strList = catList.map(returnStatement);
+console.log(strList); // ["catA's age is 10", "catB's age is 20"];
+
+// return ageList of a cat array
+const ageList = getArrByKey("age")(catList);
+console.log(ageList);
